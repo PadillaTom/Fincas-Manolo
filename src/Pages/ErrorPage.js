172dc32222af1a4b0a1d3ce0496ac0d4a8f3e27f
Tomas Padilla <padillatomasagustin@gmail.com>
2021-01-18 18:47:16 +0100

    SASS and ERROR PAGE

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom';
 const ErrorPage = () => {
   return (
     <>
-      <div className='section'>
+      <div className='section err-sect'>
         <div className='section-center'>
           <div className='error-container'>
+            <span className='numbers'>404</span>
             <h1>Pagina No Encontrada</h1>
             <Link to='/'>
-              <p className='error-cta'>
-                Regresar al <span>Inicio</span>
-              </p>
+              <div className='error-cta'>
+                <p>Volver al Inicio</p>
+              </div>
             </Link>
           </div>
         </div>
