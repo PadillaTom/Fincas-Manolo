46537166dd996271d57b54911ae8c80e6beadcab
Tomas Padilla <padillatomasagustin@gmail.com>
2021-01-19 12:46:08 +0100

    Sidebar and SASS

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AiOutlineMail,
+  AiOutlinePhone,
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+} from 'react-icons/ai';
 // Data:
 import { links } from '../Utils/constants';
 // Context:
@@ -7,16 +14,52 @@ import { useProductsContext } from '../Context/products_context';
 
 const Sidebar = () => {
   // Hook Context
-  const { isSidebarOpen } = useProductsContext();
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
 
   return (
     <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
-      <div className='sidebar-header'>
-        <h1>Sidebar</h1>
-      </div>
       {/* Links */}
+      <ul className='side-links'>
+        {links.map((link) => {
+          const { id, name, url } = link;
+          return (
+            <li key={id}>
+              <Link to={`${url}`} onClick={closeSidebar}>
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <hr />
       {/* Contact Mail */}
+      <div className='side-help'>
+        <h2>¿Necesitas Ayuda?</h2>
+        <div className='side-help-container'>
+          <div className='side-help-item'>
+            <div className='side-help-icon'>
+              <AiOutlineMail></AiOutlineMail>
+            </div>
+            <p>Fincas-Manolo@email.com</p>
+          </div>
+          <div className='side-help-item'>
+            <div className='side-help-icon'>
+              <AiOutlinePhone></AiOutlinePhone>
+            </div>
+            <p>+41 79 400 26 93</p>
+          </div>
+        </div>
+      </div>
+      <hr />
       {/* Social */}
+      <div className='side-social'>
+        <h2>Síguenos en:</h2>
+        <div className='side-social-container'>
+          <AiOutlineFacebook></AiOutlineFacebook>
+          <AiOutlineInstagram></AiOutlineInstagram>
+          <AiOutlineTwitter></AiOutlineTwitter>
+        </div>
+      </div>
     </aside>
   );
 };
